f80443de937e48974a689f44253c700659a8ca3f
Mike Mulchrone <mikemulchrone987@gmail.com>
2024-10-12 09:16:06 -0700

    #22 join results

--- cas-dotnet-benchmarks/Program.cs
+++ cas-dotnet-benchmarks/Program.cs
@@ -1,8 +1,13 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
 using cas_dotnet_benchmarks;
 using cas_dotnet_benchmarks.PasswordHashers;
 
+var config = ManualConfig.Create(DefaultConfig.Instance)
+         .With(ConfigOptions.JoinSummary)
+         .With(ConfigOptions.DisableLogFile);
+
 BenchmarkRunner.Run(new[]{
-            BenchmarkConverter.TypeToBenchmarks( typeof(PasswordHasherBenchmarks)),
-            BenchmarkConverter.TypeToBenchmarks( typeof(BCryptBenchmark))
+            BenchmarkConverter.TypeToBenchmarks( typeof(PasswordHasherBenchmarks), config),
+            BenchmarkConverter.TypeToBenchmarks( typeof(BCryptBenchmark), config)
             });
